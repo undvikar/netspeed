0774f4057799b01dc29f83dac55672cdfc4d8bcc
Dominik <disbiod@protonmail.com>
2020-11-27 22:55:42 +0100

    Arrows instead of words to indicate up- and download.

--- netspeed.c
+++ netspeed.c
@@ -80,7 +80,7 @@ main(){
     fprintf(rec, "%llu\n", received);
     fprintf(transm, "%llu\n", transmitted);
 
-    printf("DOWN: %s UP: %s \n", downspeedstr, upspeedstr);
+    printf(" %s  %s \n", downspeedstr, upspeedstr);
     fclose(rec);
     fclose(transm);
 }
