16298baaae8c1773723c6bf703beb67dcdeba2e2
Dominik <disbiod@protonmail.com>
2021-02-02 16:58:11 +0100

    config file for more interfaces

---
+++ config.h
@@ -0,0 +1,7 @@
+/* show cumulative speed of all interfaces (loopback excluded) */
+static const int cumulative = 0;        
+/* specify interfaces whose speed should be displayed separately */
+static const char *interfaces = { "enp3s0", "proton0", NULL };
+
+#define RECFILE
+/* TODO: implement file logic for more interfaces */
